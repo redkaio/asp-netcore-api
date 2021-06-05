64f18cf171f5d86abe686495f0789e1b8a4866ce
redkaio <chrno.92@gmail.com>
2021-06-04 22:26:45 -0300

    feat: :sparkles: Adicionado Projeto Angular SPA

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -46,6 +46,7 @@ namespace ProAgil.API
                 app.UseHsts();
             }
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
